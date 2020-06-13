cd20e472d6a132abfa0a3820b8aee8cc907ff735
Manik <rockonmanik@gmail.com>
2020-06-13 15:29:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// WIll handle socket io connections
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+const users= {};
+
+io.on('connection', socket=>{
+	socket.on('new-user-joined', name =>{
+		users[socket.id] = name;
+		socket.broadcast.emit('user-joined',name);
+	});
+	socket.on('send',message=>{
+		socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+	});
+
+
+	socket.on('disconnect',message=>{
+		socket.broadcast.emit('left',users[socket.id]);
+		delete users[socket.id];
+	});
+
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
